d4139159f58133cc91ac70041b3698649e48af37
grvx <spamgauravjha@gmail.com>
2025-10-20 14:28:02 +0530

    initial commit

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js"
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js"
+
+const firebaseConfig = {
+	apiKey: "AIzaSyA_rDZmlS-UTE5Ea2JuVquc7owh1cAi1nQ",
+	authDomain: "fitmart-grvx.firebaseapp.com",
+	projectId: "fitmart-grvx",
+	storageBucket: "fitmart-grvx.firebasestorage.app",
+	messagingSenderId: "405165700561",
+	appId: "1:405165700561:web:e8fd3516cf0e98f40f9cf0"
+}
+
+export const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const provider = new GoogleAuthProvider()
